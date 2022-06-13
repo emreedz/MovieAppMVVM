b9cf3f2658f044773ee6dc88be8a504ad604a90f
Kurtulan_Muhasebe <emre95.deniz@gmail.com>
2022-06-13 17:43:52 +0300

    BottomNav

---
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.internal.NavigationMenu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavController navigationMenu = Navigation.findNavController(this, R.id.nav_host);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navigationMenu);
+    }
+}
